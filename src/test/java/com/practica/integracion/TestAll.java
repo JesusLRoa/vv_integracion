1cb72e6236e1853326e5c4a3779da225d6c983c1
Jesus <92719869+JesusLRoa@users.noreply.github.com>
2021-12-12 23:47:42 +0100

    Delete TestAll.java

--- src/test/java/com/practica/integracion/TestAll.java
+++
@@ -1,8 +0,0 @@
-import com.practica.integracion.*;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
-
-@RunWith(JUnitPlatform.class)
-@SelectClasses({TestInvalidUser.class, TestValidUser.class})
-public class TestAll {}
